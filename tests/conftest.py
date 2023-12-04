ed1f0ecbfba4e262eaaedecbb95c76228e5cbecf
bj00rn@users.noreply.github.com <bj00rn@users.noreply.github.com>
2023-12-04 16:07:35 +0100

    chore(ci): implement test server

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
 """
-    Dummy conftest.py for pysaleryd.
+    Configure testing
 """
 import asyncio
 
-import aiohttp
 import pytest
 import pytest_asyncio
 from aiohttp import web
+from utils.test_server import WebsocketView
 
 
 @pytest.fixture(scope="session")
@@ -18,29 +18,9 @@ def event_loop():
     loop.close()
 
 
-async def websocket_handler(request):
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-
-    async def cleanup():
-        await ws.close(code=aiohttp.WSCloseCode.GOING_AWAY, message="Server shutdown")
-
-    try:
-        async for msg in ws:
-            if msg.type == aiohttp.WSMsgType.TEXT:
-                while True:
-                    await ws.send_str("#MF: 1+ 0+ 2+1\r")
-                    await asyncio.sleep(0.5)
-    finally:
-        await asyncio.shield(cleanup())
-
-    return ws
-
-
 @pytest_asyncio.fixture()
-async def ws_server(aiohttp_server: web.Server):
+async def ws_server(aiohttp_server: "web.Server"):
     """Websocket test server"""
     app = web.Application()
-
-    app.add_routes([web.get("/", websocket_handler)])
+    app.add_routes([web.view("/", WebsocketView)])
     return await aiohttp_server(app, port=3001)
