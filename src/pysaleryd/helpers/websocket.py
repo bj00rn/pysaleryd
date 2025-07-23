d733e568f6f594d3925eef15531eeda65f49bad6
bj00rn@users.noreply.github.com <bj00rn@users.noreply.github.com>
2025-07-23 15:31:52 +0000

    feat(websocket): add keepalive pong task and improve coroutine error handling
    Add periodic keepalive PONG messages to maintain websocket connection
    Use async context manager for task_manager
    Improve error handling and logging for coroutines

--- src/pysaleryd/helpers/websocket.py
+++ src/pysaleryd/helpers/websocket.py
@@ -4,7 +4,7 @@ import asyncio
 import logging
 from typing import Callable, Coroutine
 
-from websockets.asyncio.client import ClientConnection, connect, process_exception
+from websockets.asyncio.client import ClientConnection, connect
 from websockets.exceptions import ConnectionClosed
 from websockets.protocol import State
 
@@ -36,12 +36,13 @@ class ReconnectingWebsocketClient:
         self._ws = None
 
     @property
-    def state(self):
+    def state(self) -> State | None:
         """State of connection"""
         if self._ws:
             return self._ws.protocol.state
+        return None
 
-    async def _do_on_state_change(self):
+    async def __do_on_state_change(self):
         try:
             if callable(self._on_state_change):
                 if isinstance(result := self._on_state_change(self.state), Coroutine):
@@ -49,7 +50,7 @@ class ReconnectingWebsocketClient:
         except BaseException:
             _LOGGER.exception("Error calling on_state_change")
 
-    async def _do_on_message(self, message: str):
+    async def __do_on_message(self, message: str):
         try:
             if callable(self._on_message):
                 if isinstance(result := self._on_message(message), Coroutine):
@@ -57,7 +58,7 @@ class ReconnectingWebsocketClient:
         except BaseException:
             _LOGGER.exception("Error calling on_message")
 
-    async def _do_on_connect(self):
+    async def __do_on_connect(self):
         try:
             if callable(self._on_connect):
                 if isinstance(result := self._on_connect(), Coroutine):
@@ -69,62 +70,59 @@ class ReconnectingWebsocketClient:
         """Add message to send queue"""
         await self._outgoing_queue.put(message)
 
-    async def runner(self):
+    async def __runner(self):
         """Send and receive messages on websocket"""
         try:
             uri = f"ws://{self._host}:{self._port}"
             _LOGGER.info("Connecting to %s", uri)
-            async for websocket in connect(uri, open_timeout=self._connect_timeout):
+            async for websocket in connect(
+                uri, open_timeout=self._connect_timeout, ping_interval=None
+            ):
                 try:
                     self._ws = websocket
                     _LOGGER.info("Connection established to %s", uri)
-                    await self._do_on_connect()
-                    await self._do_on_state_change()
+                    await self.__do_on_connect()
+                    await self.__do_on_state_change()
                     async with task_manager(cancel_on_exit=True) as ws_tasks:
+                        pong_task = asyncio.create_task(
+                            self.__keepalive(websocket), name="pong"
+                        )
                         consumer_task = asyncio.create_task(
-                            self._consumer(websocket), name="consumer"
+                            self.__consumer(websocket), name="consumer"
                         )
                         producer_task = asyncio.create_task(
-                            self._producer(websocket), name="producer"
+                            self.__producer(websocket), name="producer"
                         )
-                        ws_tasks.add(consumer_task, producer_task)
+                        ws_tasks.add(consumer_task, producer_task, pong_task)
                         await ws_tasks.wait(return_when=asyncio.FIRST_COMPLETED)
-                except BaseException as e:  # pylint: disable=W0718
-                    if isinstance(e, ConnectionClosed):
-                        _LOGGER.warning(
-                            "Connection to %s closed by remote host: %s, will retry",
-                            uri,
-                            e.reason,
-                        )
-                        continue
-                    elif process_exception(e) is None:
-                        _LOGGER.warning(
-                            "Connection to %s failed due to transient error, will retry",  # noqa: E501
-                            uri,
-                            exc_info=1,
-                        )
-                        continue
-                    else:
-                        raise
+                except ConnectionClosed as e:  # pylint: disable=W0718
+                    _LOGGER.warning(
+                        "Connection to %s closed by remote host: %s, will retry",
+                        uri,
+                        e.reason,
+                    )
+                    continue
+                except Exception as e:
+                    _LOGGER.error("Error occurred in websocket: %s", e)
+                    raise
                 finally:
-                    await self._do_on_state_change()
-
+                    await self.__do_on_state_change()
         except asyncio.CancelledError:
             _LOGGER.debug("Shutting down connection to %s", uri)
             raise
 
-    async def _consumer(self, ws: ClientConnection):
+    async def __consumer(self, ws: ClientConnection):
         """Enqueue messages received on websocket"""
         try:
             async for message in ws:
                 if isinstance(message, str):
                     _LOGGER.debug("Message received %s", message)
-                    await self._do_on_message(message)
+                    await self.__do_on_message(message)
         except asyncio.CancelledError:
             _LOGGER.debug("Consumer was cancelled")
             raise
 
-    async def _producer(self, ws: ClientConnection):
+    async def __producer(self, ws: ClientConnection):
         """Send queued messages on websocket"""
         try:
             while True:
@@ -136,10 +134,20 @@ class ReconnectingWebsocketClient:
             _LOGGER.debug("Producer cancelled")
             raise
 
+    async def __keepalive(self, websocket, pong_interval=float(30)):
+        while True:
+            await asyncio.sleep(pong_interval)
+            try:
+                _LOGGER.debug("Sending keepalive PONG")
+                await websocket.send("PONG\r")
+            except ConnectionClosed:
+                _LOGGER.warning("Connection closed during keepalive, stopping")
+                raise
+
     async def connect(self):
         """Connect to server"""
         if not self._tasks:
-            runner_task = asyncio.create_task(self.runner(), name="runner")
+            runner_task = asyncio.create_task(self.__runner(), name="runner")
             self._tasks.add(runner_task)
         else:
             _LOGGER.warning("Already connected to %s:%s", self._host, self._port)
@@ -147,7 +155,6 @@ class ReconnectingWebsocketClient:
     def close(self):
         """Close connection and perform clean up"""
         self._tasks.cancel()
-        self._ws = None
 
     def __enter__(self):
         self.connect()
